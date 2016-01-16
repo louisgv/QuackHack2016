22fab1a654bd07e32988d70adee5b106f6b97eb4
Louis Graham Vichy <lgvichy@gmail.com>
2016-01-16 13:54:16 -0800

    Added eyes and bouncy animation

---
+++ Assets/Eyes.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eyes : MonoBehaviour
+{
+	
+	public GameObject target;
+	
+	public GameObject leftEye;
+	
+	public GameObject rightEye;
+	
+	public GameObject leftPupil;
+	
+	public GameObject rightPupil;
+	
+	public float eyeRadius;
+	
+	public float pupilRadius;
+	
+	public enum EyesState
+	{
+		TRACKING,
+		IDLE
+	}
+	
+	public EyesState state = EyesState.TRACKING;
+	
+	Vector3 CalculateFinalPupilPosition (Vector3 pupilPosition, Vector3 eyePosition)
+	{
+		Vector3 distanceToTarget = target.transform.position - pupilPosition;
+		
+		distanceToTarget = Vector3.ClampMagnitude (distanceToTarget, eyeRadius - pupilRadius);
+		
+		return eyePosition + distanceToTarget;
+	}
+	
+	void MovePupils ()
+	{
+		if (state.Equals (EyesState.TRACKING)) {
+			Vector3 finalLeftPupilPosition = CalculateFinalPupilPosition (leftPupil.transform.position, leftEye.transform.position);
+		
+			Vector3 finalRightPupilPosition = CalculateFinalPupilPosition (rightPupil.transform.position, rightEye.transform.position);
+		
+			leftPupil.transform.position = Vector3.Lerp (leftPupil.transform.position, finalLeftPupilPosition, Time.fixedDeltaTime);
+		
+			rightPupil.transform.position = Vector3.Lerp (rightPupil.transform.position, finalRightPupilPosition, Time.fixedDeltaTime);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		MovePupils ();
+	}
+}
--- Assets/Scritps/Building.cs
+++ Assets/Scritps/Building.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Building : MonoBehaviour {
+public class Building : MonoBehaviour
+{
 
 	public float health;
 	public float construction_speed;
@@ -12,18 +13,20 @@ public class Building : MonoBehaviour {
 	public Mob target;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 
-	void attack() {
+	void attack ()
+	{
 		// Iterate through NPCs, select those within attack distance, attack is attack speed has refreshed
-		Mob[] attackableTargets = FindObjectsOfType (typeof(Mob)) as Mob[] ;
+		Mob[] attackableTargets = FindObjectsOfType (typeof(Mob)) as Mob[];
 		foreach (Mob attackable_target in attackableTargets) {
 			Debug.Log (attackable_target);
 			if (Vector3.Distance (target.transform.position, transform.position) < attack_distance) {
 				Debug.Log ("Building within range");
-				if (last_attack_time+attack_speed < Time.time) {
+				if (last_attack_time + attack_speed < Time.time) {
 					attackable_target.takeDamage (attack_damage);
 					last_attack_time = Time.time;
 				}
@@ -31,7 +34,8 @@ public class Building : MonoBehaviour {
 		} 
 	}
 
-	void onDie() {
+	void onDie ()
+	{
 		// Change stats and sprite
 		construction_speed = 0;
 		attack_damage = 0;
@@ -39,9 +43,10 @@ public class Building : MonoBehaviour {
 		attack_distance = 0;
 	}
 
-	public void takeDamage(float damage) {
+	public void takeDamage (float damage)
+	{
 		// Called when an NPC attacks
-		Debug.Log("Building took damage");
+		Debug.Log ("Building took damage");
 		if (health > 0) {
 			health -= damage;
 		} else if (health <= 0) {
@@ -51,8 +56,9 @@ public class Building : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		Debug.Log(health);
+	void FixedUpdate ()
+	{
+		Debug.Log (health);
 	}
 
 }
